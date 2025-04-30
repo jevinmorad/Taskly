21fde2f264464387ae7667e82651b41b23284572
jevinmorad <23010101171@darshan.ac.in>
2025-04-30 10:34:10 +0530

    refactor: remove unnecessarily log

--- server/models/User.js
+++ server/models/User.js
@@ -36,9 +36,6 @@ userSchema.pre('save', async function (next) {
 
 // Method to compare entered password with hashed password in DB
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    console.log("typeof entered password: ", typeof enteredPassword);
-    console.log("typeof this.password: ", this.password);
-
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
