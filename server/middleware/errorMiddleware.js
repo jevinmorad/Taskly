86bd6d41e4ce5b75345459d2fd2e29b9dca8a457
jevinmorad <23010101171@darshan.ac.in>
2025-04-30 10:30:47 +0530

    fix: syntax error in getting url

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -11,7 +11,7 @@ const errorHandler = (err, req, res, next) => {
 }
 
 const notFound = (req, res, next) => {
-    const error = new Error('Not found - ', err.originalUrl);
+    const error = new Error('Not found - ', req.originalUrl);
     res.status(404);
     next(error);
 }
