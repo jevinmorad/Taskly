2e7acad4ca67522960c51fdd1b2ee9b2cf9cd466
jevinmorad <23010101171@darshan.ac.in>
2025-05-01 14:57:01 +0530

    refactor: log statement

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-    console.log(`erver running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
