c5d7d1e49af12b48c993fbed3e95615e2d887fca
jevinmorad <23010101171@darshan.ac.in>
2025-05-02 22:13:11 +0530

    refactor: new design of navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import { HelpCircle, Search, Settings } from "lucide-react";
 import ThemeToggleButton from "./ThemeToggleButton";
+import AuthContext from "../context/AuthContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+    const { user, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
     return (
-        <header className="w-full h-12 flex items-center justify-between px-4 bg-purple-header text-white dark:bg-zinc-900 border-b border-1 border-medium-gray-2">
+        <header className="w-full h-12 flex items-center justify-between px-4 bg-purple-header text-white dark:bg-zinc-900 border-b border-1 border-gray-50">
+
+            {/* Logo/Title */}
             <div className="font-semibold text-large">To Do</div>
+
+            {/* Searchbar */}
             <div className="flex-1 flex justify-center">
                 <div className="relative w-[30%]">
                     <input type="text" placeholder="Search" className="w-full text-black dark:bg-zinc-800 dark:text-white text-sm rounded-md pl-8 pr-3 py-1.5 focus:outline-none focus:ring-1 focus:ring-blue-400" />
-                    <Search className="absolute text-forest-green dark:text-mint-selected left-2 top-1/2 -translate-y-1/2 text-purple-tex" size={16} />
+                    <Search className="absolute text-green-800 dark:text-blue-400 left-2 top-1/2 -translate-y-1/2" size={16} />
                 </div>
             </div>
+
+            {/* Accessibility */}
             <div className="flex items-center space-x-4 text-black">
                 <Settings className="text-white" size={18} />
                 <HelpCircle className="text-white" size={18} />
